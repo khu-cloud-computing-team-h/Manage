8b272c6d34d78837af3da3a91bd3422ca0ea4923
junghyunseok <jhyunseok325@gmail.com>
2024-05-24 21:29:46 +0900

    Create: Tag Entity 구현

---
+++ ccjhs/src/main/java/cloudcomputing/jhs/Tag/Tag.java
@@ -0,0 +1,17 @@
+package cloudcomputing.jhs.Tag;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TagID")
+    private Long tagID;
+
+    @Column(name = "TagName")
+    private String tagName;
+}
