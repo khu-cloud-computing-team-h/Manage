67390d098e2b8f7c2f7e30495a95a04488711d07
junghyunseok <jhyunseok325@gmail.com>
2024-05-25 01:50:31 +0900

    refactor: S3 구현에 따른 로직 변경

--- ccjhs/src/main/java/cloudcomputing/jhs/Image/ImageController.java
+++ ccjhs/src/main/java/cloudcomputing/jhs/Image/ImageController.java
@@ -1,5 +1,6 @@
 package cloudcomputing.jhs.Image;
 
+import cloudcomputing.jhs.S3.S3Service;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.apache.coyote.Response;
@@ -23,46 +24,28 @@ public class ImageController {
     @Autowired
     private ImageService imageService;
 
+    @Autowired
+    private S3Service s3Service;
+
     @PostMapping("/api/manage/image")
     public ResponseEntity<String> uploadImage(@RequestParam("userID") String userID, @RequestParam("imageFile") MultipartFile imageFile) {
 
-        //이미지를 저장할 디렉토리 경로 설정
-        String uploadDir = "C:\\Users\\jhs\\Desktop\\경희대학교\\2024-1학기\\클라우드컴퓨팅\\CloudComputing-project\\images\\" + userID + "/";
-        File directory = new File(uploadDir);
-
-        if (!directory.exists()) {
-            directory.mkdirs();
-        }
-
         try {
-            //이미지 파일의 원본 파일 이름
-            String originalFileName = imageFile.getOriginalFilename();
-            //이미지 파일의 확장자 추출
-            String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
-
-            //이미지 ID 생성(현재 시간을 사용하여 고유한 ID 생성)
-            Long imageID = System.currentTimeMillis();
-            //파일명은 이미지 ID + 확장자로 설정
-            String fileName = imageID + fileExtension;
-
-            //이미지를 저장할 경로 설정
-            Path uploadPath = Paths.get(uploadDir + fileName);
-            //이미지 디스크에 저장
-            Files.write(uploadPath, imageFile.getBytes());
+            //S3에 이미지 업로드
+            String imageUrl = s3Service.uploadFile(imageFile);
 
             //MySQL에 이미지 정보 저장
             BigDecimal userIdBigDecimal = new BigDecimal(userID);
-            imageService.saveImage(imageID, userIdBigDecimal, uploadPath.toString());
+            //imageID는 자동 생성
+            imageService.saveImage(null, userIdBigDecimal, imageUrl);
 
-            //이미지 저장 후 이미지 ID 반환
-            return ResponseEntity.ok().body(imageID.toString());
+            //이미지 저장 후 이미지 URL 반환
+            return ResponseEntity.ok().body("Image upload success. Image URL: " + imageUrl);
 
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body("Image upload Fail");
         }
-
-
     }
 
     @GetMapping("/api/manage/image/{imageID}")
